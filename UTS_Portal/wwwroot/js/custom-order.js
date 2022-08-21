331e5ed2b7346e5cd04fae8cf34aece7919847a0
ThienHoa <hoatt1097@gmail.com>
2022-08-21 09:35:25 +0700

    UPdate menu order

---
+++ UTS_Portal/wwwroot/js/custom-order.js
@@ -0,0 +1,93 @@
+﻿jQuery(document).ready(function () {
+    $('input[type="radio"]').on('change', function () {
+        var name = $(this).attr('name');
+
+        $("input[name=" + name + "]").not(':checked').each(function () {
+            var func = 'removeClass';
+            $('label[for="' + $(this).attr('id') + '"]')[func]('checked');
+            $('label[for="hide-' + $(this).attr('id') + '"]')[func]('checked');
+        });
+
+        var func = 'addClass';
+        $('label[for="' + $(this).attr('id') + '"]')[func]('checked');
+        $('label[for="hide-' + $(this).attr('id') + '"]')[func]('checked');
+
+
+        // Auto check all option order
+        const NameInputArr = name.split("-");
+        var NameOptionCb = NameInputArr[0] + "-op-" + NameInputArr[1]; // breakfast-op-@day.Day
+        $('input[name="' + NameOptionCb + '"]').each(function () {
+            $(this).prop('checked', 'checked');
+
+            var func = 'addClass';
+            $('label[name="' + NameOptionCb + '"]')[func]('checked');
+            $('label[name="hide-' + NameOptionCb + '"]')[func]('checked');
+        });
+
+    });
+
+
+    // Init Language
+    if ($("#language").val() === "vn") {
+        $(".title-en").hide();
+        $(".en").hide();
+        $(".en").each(function () {
+            var value = $(this).attr('for');
+            $(this).attr('for', "hide-" + value);
+        });
+    }
+    else {
+        $(".title-vn").hide();
+        $(".vn").hide();
+        $(".vn").each(function () {
+            var value = $(this).attr('for');
+            $(this).attr('for', "hide-" + value);
+        });
+    }
+    
+
+
+    // Language handle
+    $('#language').on('change', function (e) {
+        var language = $("#language").val();
+        if (language === "vn") {
+            $(".title-en").hide();
+            $(".title-vn").show();
+
+            $(".en").hide();
+            $(".en").each(function () {
+                var value = $(this).attr('for');
+                if (value.indexOf("hide-") == -1) {
+                    $(this).attr('for', "hide-" + value);
+                }
+            });
+
+            $(".vn").show();
+            $(".vn").each(function () {
+                var value = $(this).attr('for');
+                if (value.indexOf("hide-") != -1) {
+                    $(this).attr('for', value.replace("hide-", ""));
+                }
+            });
+        } else {
+            $(".title-vn").hide();
+            $(".title-en").show();
+
+            $(".vn").hide();
+            $(".vn").each(function () {
+                var value = $(this).attr('for');
+                if (value.indexOf("hide-") == -1) {
+                    $(this).attr('for', "hide-" + value);
+                }
+            });
+
+            $(".en").show();
+            $(".en").each(function () {
+                var value = $(this).attr('for');
+                if (value.indexOf("hide-") != -1) {
+                    $(this).attr('for', value.replace("hide-", ""));
+                }
+            });
+        }
+    });
+});
