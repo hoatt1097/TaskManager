b296795997bbfe3a9b1b0d15db530fdca62124ef
ThienHoa <hoatt1097@gmail.com>
2022-09-09 18:12:50 +0700

    Update history page

--- UTS_Portal/Controllers/OrdersController.cs
+++ UTS_Portal/Controllers/OrdersController.cs
@@ -54,65 +54,93 @@ namespace UTS_Portal.Controllers
         public IActionResult History(string? parentId, string? month)
         {
             var currentUser = UserHelper.GetCurrentUser(HttpContext);
-            if (currentUser.RoleName == "Parent")
+            if (currentUser.RoleName?.Trim() == "Parent")
             {
-                if(currentUser.Code != parentId)
+                if(currentUser.Code?.Trim() != parentId?.Trim())
                 {
                     _notyfService.Error("User has not permission!");
                     return RedirectToAction("Index", "Home");
                 }
 
                 // Defaul show data
-                ViewBag.ParentId = currentUser.Code;
-                ViewBag.StudentName = currentUser.Username;
+                ViewBag.ParentId = currentUser.Code?.Trim();
+                ViewBag.StudentName = currentUser.Username?.Trim();
             }
 
             //Format month from select: MM/yyyy
             var ListParent = _context.Cscard.AsNoTracking().ToList();
-            var SelectBoxOrderMonth = BuildSelectBoxOrderMonth();
+            var SelectBoxOrderMonth = BuildSelectBoxOrderMonth(month);
             ViewBag.SelectBoxOrderMonth = SelectBoxOrderMonth;
 
-            if (parentId != null && month != null)
+            if (parentId?.Trim() != null && month?.Trim() != null)
             {
                 List<OrderSubmit> listOrderSubmit = new List<OrderSubmit>();
+
                 var orderHistory = _context.PreOrders
-                    .Where(x => x.UserCode == parentId && x.MonthYear == month.Replace("/", ""))
+                    .Where(x => x.UserCode.Trim() == parentId.Trim() && x.MonthYear == month.Replace("/", ""))
                     .ToList().OrderBy(x => x.OrderDate).ToList();
+
                 var listOrderDate = orderHistory.Select(x => x.OrderDate).Distinct();
                 foreach(var day in listOrderDate)
                 {
+                    int Day = day.Day;
+                    string CurrentMonth = month;
+                    List<OrderItem> Breakfast = new List<OrderItem>();
+                    List<OrderItem> Lunch = new List<OrderItem>();
+                    List<OrderItem> Afternoon = new List<OrderItem>();
+
                     var listOrderByDate = orderHistory.Where(x => x.OrderDate == day).ToList();
-                    foreach(var preOrder in listOrderByDate)
+                    foreach(var item in listOrderByDate)
                     {
-                        OrderSubmit orderSubmit = new OrderSubmit();
 
-                        Goods goods = _context.Goods.Where(x => x.GoodsId == preOrder.ItemCode).FirstOrDefault();
+                        Goods goods = _context.Goods.Where(x => x.GoodsId == item.ItemCode).FirstOrDefault();
 
-                        int Day = day.Day;
-                        string CurrentMonth = month;
-                        List<OrderItem> Breakfast = new List<OrderItem>();
-                        List<OrderItem> Lunch = new List<OrderItem>();
-                        List<OrderItem> Afternoon = new List<OrderItem>();
                         OrderItem orderItem = new OrderItem
                         {
-                            Code = preOrder.ItemCode,
-                            Ckcode = preOrder.CkCode,
-                            OriginName = goods != null ? goods.OtherName : "",
-                            NameVn = goods != null ? goods.EnName : "",
-                            NameEn = goods != null ? goods.FullName : "",
-                            Qty = preOrder.Qty,
-                            Bundled = preOrder.IsBundled == true ? 1 : 0,
-                            RepastId = preOrder.RepastId
+                            Code = item.ItemCode?.Trim(),
+                            Ckcode = item.CkCode?.Trim(),
+                            OriginName = goods != null ? goods.OtherName?.Trim() : "",
+                            NameVn = goods != null ? goods.FullName?.Trim() : "",
+                            NameEn = goods != null ? goods.EnName?.Trim() : "",
+                            Qty = item.Qty,
+                            Bundled = item.IsBundled == true ? 1 : 0,
+                            RepastId = item.RepastId
                         };
-                    }
-                    
 
+                        if(orderItem.RepastId == 1)
+                        {
+                            Breakfast.Add(orderItem);
+                        }
+
+                        if (orderItem.RepastId == 2)
+                        {
+                            Lunch.Add(orderItem);
+                        }
+
+                        if (orderItem.RepastId >= 3)
+                        {
+                            Afternoon.Add(orderItem);
+                        }
+                    }
+
+                    OrderSubmit orderDay = new OrderSubmit
+                    {
+                        Day = Day,
+                        CurrentMonth = CurrentMonth,
+                        Breakfast = Breakfast,
+                        Lunch = Lunch,
+                        Afternoon = Afternoon
+                    };
+
+                    listOrderSubmit.Add(orderDay);
                 }
 
                 // Defaul show data
                 var parent = _context.Cscard.AsNoTracking().Where(x => x.ParentId == parentId).FirstOrDefault();
-                ViewBag.ParentId = parent?.ParentId;
-                ViewBag.StudentName = parent?.Name;
+                ViewBag.ParentId = parent?.ParentId?.Trim();
+                ViewBag.StudentName = parent?.Name?.Trim();
+
+                ViewBag.ListOrderSubmit = listOrderSubmit;
             }
             
             return View();
@@ -149,8 +177,8 @@ namespace UTS_Portal.Controllers
                                 OrderDate = orderDate,
                                 SubmitDt = DateTime.Now,
                                 SubmitTm = DateTime.Now.ToString("HH:mm"),
-                                ItemCode = item.Code.Trim(),
-                                CkCode = item.Ckcode.Trim(),
+                                ItemCode = item.Code?.Trim(),
+                                CkCode = item.Ckcode?.Trim(),
                                 Qty = item.Qty,
                                 RepastId = 1,
                                 Class = "",
@@ -178,8 +206,8 @@ namespace UTS_Portal.Controllers
                                 OrderDate = orderDate,
                                 SubmitDt = DateTime.Now,
                                 SubmitTm = DateTime.Now.ToString("HH:mm"),
-                                ItemCode = item.Code.Trim(),
-                                CkCode = item.Ckcode.Trim(),
+                                ItemCode = item.Code?.Trim(),
+                                CkCode = item.Ckcode?.Trim(),
                                 Qty = item.Qty,
                                 RepastId = 2,
                                 Class = "",
@@ -207,8 +235,8 @@ namespace UTS_Portal.Controllers
                                 OrderDate = orderDate,
                                 SubmitDt = DateTime.Now,
                                 SubmitTm = DateTime.Now.ToString("HH:mm"),
-                                ItemCode = item.Code.Trim(),
-                                CkCode = item.Ckcode.Trim(),
+                                ItemCode = item.Code?.Trim(),
+                                CkCode = item.Ckcode?.Trim(),
                                 Qty = item.Qty,
                                 RepastId = 3,
                                 Class = "",
@@ -249,11 +277,11 @@ namespace UTS_Portal.Controllers
 
             return selectElm;
         }
-        public SelectList BuildSelectBoxOrderMonth()
+        public SelectList BuildSelectBoxOrderMonth(string? month)
         {
             var Month = _context.MenuInfos.OrderBy(x => x.Month).ToList();
             var MonthList = new List<Object>();
-            string currentMonth = DateTime.Now.ToString("MM/yyyy");
+            string currentMonth = month != null ? month : DateTime.Now.ToString("MM/yyyy");
             foreach (var m in Month)
             {
                 MonthList.Add(new { Id = m.Month.ToString("MM/yyyy"), Name = m.Month.ToString("MM/yyyy") });
